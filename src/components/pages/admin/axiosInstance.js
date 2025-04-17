e0fb24fee2f9dd0c7cfcf88036daae2f500bfce8
TranVinhTanDat <tandat27012002.td@gmail.com>
2025-04-17 22:51:39 +0700

    update deploy

--- src/components/pages/admin/axiosInstance.js
+++ src/components/pages/admin/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://e-commerceapi-uk5z.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://e-commerceapi-uk5z.onrender.com";
 
 export default API_BASE_URL;
